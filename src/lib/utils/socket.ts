0f4c1c370c7c6559bdcffb6ed983d195122667e6
paperdeer-assignment <tjrgus0713@naver.com>
2025-05-10 10:38:47 +0900

    feat: websocket 연결시 배포된 url에 맞추어 설정하는 함수 추가

--- src/lib/utils/socket.ts
+++ src/lib/utils/socket.ts
@@ -1,4 +1,21 @@
+const getWebSocketUrl = () => {
+	// 브라우저에서 실행 중일 때
+	if (typeof window !== 'undefined') {
+		const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+		const host = window.location.host; // 도메인:포트
+
+		if (import.meta.env.DEV) {
+			return 'ws://localhost:8080';
+		}
+
+		return `${protocol}//${host}`;
+	}
+
+	// 서버사이드에서 실행 중일 때
+	return '';
+};
+
 export function connectWebSocket() {
-	const ws = new WebSocket('ws://localhost:8080');
+	const ws = new WebSocket(getWebSocketUrl());
 	return ws;
 }
